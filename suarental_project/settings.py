eb3cf5ce86fdd3e626a5a9c2a5bb5af8cef9168b
Muslim-Pro <muslimsalumu127@gmail.com>
2026-08-31 18:15:12 +0300

    Remove MEDIA_ROOT to enforce Cloudinary media storage

--- suarental_project/settings.py
+++ suarental_project/settings.py
@@ -130,9 +130,14 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Picha zilizopakiwa zinaenda kwenye house_images/ na room_images/ chini ya project
+# Media Settings kwa ajili ya Cloudinary
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# Picha zilizopakiwa zinaenda kwenye house_images/ na room_images/ chini ya project
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
